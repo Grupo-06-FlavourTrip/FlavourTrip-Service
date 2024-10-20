09d6ef319a7214a34f21e66e7908313be89ceb51
DanielChirinos615 <89100997+DanielChirinos615@users.noreply.github.com>
2024-10-20 00:05:26 -0500

    first commit

---
+++ src/main/java/com/FlavourTrip/FlavourTripApplication.java
@@ -0,0 +1,13 @@
+package com.FlavourTrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlavourTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlavourTripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FlavourTrip/FlavourTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FlavourTrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlavourTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
