b95f25e9d40dd8d8d0b22d5bb394500fcf7f987b
DanielChirinos615 <89100997+DanielChirinos615@users.noreply.github.com>
2024-10-20 01:41:36 -0500

    develop works

--- src/main/java/com/FlavourTrip/user/domain/model/aggregates/DebitCard.java
+++ src/main/java/com/FlavourTrip/user/domain/model/aggregates/DebitCard.java
@@ -2,8 +2,11 @@ package com.FlavourTrip.user.domain.model.aggregates;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
+//Para que funcione el agregar user, quitar la etiqueta NoArgsConstructor y para que funcione el debit card, poner NoArgsConstructor
 @Entity
 @Getter
 @Setter
@@ -21,9 +24,6 @@ public class DebitCard {
 
     private String name;
 
-    public DebitCard() {
-
-    }
     public DebitCard(Long numCard, Long cvv, String date, String name) {
         this.numCard = numCard;
         this.cvv = cvv;
